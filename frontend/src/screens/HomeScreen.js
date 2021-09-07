73b15ff1241f1fe5d614b54c110979c8133a5b75
Greg Scott <gtscott90@gmail.com>
2021-09-07 12:54:44 -0400

    update to homescreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -13,7 +13,7 @@ export default function HomeScreen() {
     }, [])
     return (
             <div className="row center">
-            {products.map((product) => (
+            { products.map((product) => (
               <Product key={product._id} product={product}></Product>
               ))
             }
