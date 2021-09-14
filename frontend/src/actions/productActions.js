2a2d396e772550d5e3b19093fc1c0fb655c0f0c2
Greg Scott <gtscott90@gmail.com>
2021-09-14 14:09:58 -0400

    added details product constants and actions to begin adding the product page to the Redux data pathway

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,7 +1,15 @@
 // defining actions as functions
 
 import axios from "axios";
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
+import { 
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_REQUEST, 
+    PRODUCT_DETAILS_SUCCESS, 
+    PRODUCT_LIST_FAIL, 
+    PRODUCT_LIST_REQUEST, 
+    PRODUCT_LIST_SUCCESS 
+} from "../constants/productConstants"
+import ProductScreen from "../screens/ProductScreen";
 
 // this function returns another function
 export const listProducts = () => async(dispatch) => {
@@ -17,5 +25,25 @@ export const listProducts = () => async(dispatch) => {
         // dispatch the fail scenario
         dispatch({type: PRODUCT_LIST_FAIL, payload: error.message});
     }
-    
 };
+
+export const detailsProduct = (productId) => async(dispatch) => {
+    dispatch({
+        type: PRODUCT_DETAILS_REQUEST, payload: productId
+    });
+    try {
+        const { data } = axios.get(`api/products/${productId}`);
+        dispatch({type: PRODUCT_DETAILS_SUCCESS, payload: data});
+    } catch(error) {
+        // check for error.response, if it exists then backend failed error message
+        // show error.response instead of general error message of error.message
+        // otherwise show the gnereal error of error.message
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL, 
+            payload: 
+            error.response && error.response.data.message 
+            ? error.response.data.message
+            : error.message
+        });
+    }
+}
