7f3e85d859b08a726085f59d288c44176a5da9fb
Greg Scott <gtscott90@gmail.com>
2021-09-14 14:18:31 -0400

    added product details reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,10 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import { 
+    PRODUCT_DETAILS_REQUEST, 
+    PRODUCT_DETAILS_SUCCESS, 
+    PRODUCT_LIST_FAIL, 
+    PRODUCT_LIST_REQUEST, 
+    PRODUCT_LIST_SUCCESS 
+} from "../constants/productConstants";
 
 // this reducer is a function that accepts two parameters, state must have a default of products as an emptry array to avoid error
 export const productListReducer = (
@@ -15,6 +21,18 @@ export const productListReducer = (
             return { loading: false, error: action.payload }
         default:
             // returning current state
+            return state;
+    }
+}
+
+export const productDetailsReducter = (state = {product:{}, loading: true}, action) => {
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            // setting loading to true while sending the ajax request
+            return { loading: true };
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload }
+        default:
             return state;
     }
 }
